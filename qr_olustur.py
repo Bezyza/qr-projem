f04cf59024c93cf536aa6d2a25a004d29aad91d1
beyza <94079552+Bezyza@users.noreply.github.com>
2025-11-11 16:52:14 +0300

    Add files via upload

--- qr_olustur.py
+++ qr_olustur.py
@@ -1,10 +1,15 @@
 import qrcode
 
-url = "https://qr-projem.onrender.com/track"
+# Base URL
+base_url = "https://qr-projem.onrender.com/track"
+
+url_teknosa = f"{base_url}?qr=teknosa"
+img1 = qrcode.make(url_teknosa)
+img1.save("qr_teknosa.png")
+print("Teknosa QR oluşturuldu:", url_teknosa)
 
 
-img = qrcode.make(url)
-
-img.save("qr_kodum.png")
-
-print("QR kod başarıyla oluşturuldu: qr_kodum.png")
+url_media = f"{base_url}?qr=mediamarkt"
+img2 = qrcode.make(url_media)
+img2.save("qr_mediamarkt.png")
+print("MediaMarkt QR oluşturuldu:", url_media)
